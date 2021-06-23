07919a313b0c5d1bab784370918e0d1d5d5f2b14
YonatanG <yonatangr86@gmail.com>
2021-06-23 23:42:14 +0300

    mongodb added

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const port = 5000;
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+dotenv.config();
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    // { useUnifiedTopology: true },
+    () => {
+        console.log("connected to DB");
+    }
+);
+
+
 app.get("/", (req, res) => res.json("Welcome to the server"));
 
 app.listen(port, () => {
